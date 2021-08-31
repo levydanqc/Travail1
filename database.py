deaab5b8146de8f278f7e8853cfa17a81126d46d
Dan Levy <danlevy.ca@icloud.com>
2021-08-31 11:44:17 -0400

    Ajout de la fonction de lecture des données.

--- database.py
+++ database.py
@@ -4,17 +4,19 @@ import os.path as path
 
 def readData(fileName: str):
     """ Read file data and return tuple if file is valid
-        
+
         Args
         ----
         fileName (str): Name of the file where the data is stored
     """
     if path.isfile(fileName) and path.splitext(fileName) == "csv":
-        pass
+        with open("produits.csv") as file:
+            return [tuple(i.replace('\n', '').split(';')) for i in file]
 
 
 def printData():
-    pass
+    with open("produits.csv") as file:
+        print([tuple(i.replace('\n', '').split(';')) for i in file][0])
 
 
 if __name__ == "__main__":
