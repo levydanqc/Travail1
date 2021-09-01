095f3dafc8d8af3f712a30d7a7b9dc987b94be9c
Dan Levy <danlevy.ca@icloud.com>
2021-09-01 13:25:38 -0400

    Corrections d'erreurs.

--- database.py
+++ database.py
@@ -4,18 +4,18 @@ import os.path as path
 
 def validate(data: tuple):
     """ Validation des données à l'aide de règles prédéfinies.
-        Retourne Vrai si les données sont invalide en fonction de:
-        - DIN est plus petit que 3$
-        - Aucune taxe fédérale ou provinciale
-        - UPC contient plus de 12 caratères (14 en comptant le ".0" du float)
-        - Prix de vente est plus petit que le prix coûtant
+        Retourne Vrai si les données sont valide en fonction de:
+        - DIN est plus grand que 3$
+        - Il y a une taxe fédérale et provinciale
+        - UPC contient moins de 12 caratères (14 en comptant le ".0" du float)
+        - Prix de vente est plus grand que le prix coûtant
 
         Args
         ----
         data (tuple): Données d'un produit
     """
-    return (int(data[-4]) < 3) or (int(data[-1]) == int(data[-2]) == 0) \
-        or (len(str(float(data[1]))) < 14) or (float(data[7]) < float(data[6]))
+    return (int(data[-1]) == int(data[-2]) == 1) and (int(data[-4]) > 3) \
+        and (len(str(float(data[1]))) <= 14) and (float(data[7]) > float(data[6]))
 
 
 def read_data(file_name: str):
@@ -28,7 +28,7 @@ def read_data(file_name: str):
     """
     if path.isfile(file_name) and path.splitext(file_name)[1] == '.csv':
         with open("produits.csv") as file:
-            return [x for i in file if not
+            return [x for i in file if
                     validate(x := tuple(i.replace(',', '.').replace('\n', '').split(';')))]
     else:
         raise ValueError("Le nom du fichier n'est pas valide.")
